f1f7fd3d2af82866dc4e1512986d787223d5a4ee
Jessie Tivey <jesstivey@gmail.com>
2020-01-23 20:13:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+end  
+
+def batch_badge_creator(attendees)
+  puts attendees
+end 
+
+def assign_rooms
+  room_assignments = [1,2,3,4,5,6,7]
+  
+end  
